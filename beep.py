7a0d0174df51db5fa9f38487f94644bb088e5759
Zemke <florian@zemke.io>
2020-11-15 00:32:40 +0100

    It makes beep bop

---
+++ beep.py
@@ -0,0 +1,23 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('beep bop')
+
+client.run(TOKEN)
+
